a3b60f4255b8eb934b205bc9c75681eaa82621cd
Patrick Beck <pbeck@yourse.de>
2013-01-27 19:25:53 +0100

    Add Atom feed, GUI changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,20 +4,11 @@
 AUTHOR = u'Frank Gau'
 SITENAME = u'epydial'
 SITEURL = 'http://epydial.pyneo.org'
-
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
-
-# Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 THEME = "epydialtheme"
